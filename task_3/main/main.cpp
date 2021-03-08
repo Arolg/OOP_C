022b02dadc13e3633b74f0df9188945d60ee826a
Arolg <ismatov.far@yandex.ru>
2021-03-08 13:31:34 +0300

    task 5

---
+++ task_3/main/main.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+
+class Figure 
+{
+public:
+    virtual void S() = 0;
+    virtual void P() = 0;
+    virtual int GetAngles() = 0;
+    virtual int GetPerimetr() = 0;
+    virtual int GetSquare() = 0;
+protected:
+    int a = 0;
+    int b = 0;
+    int s = 0;
+    int p = 0;
+    int angles = 0;
+};
+
+class Square : public Figure
+{
+public:
+    Square() {}
+    Square(int a);
+    int GetAngles() override;
+    int GetPerimetr() override;
+    int GetSquare() override;
+    void S() override;
+    void P() override;
+};
+
+class Rect : public Square
+{
+public:
+    Rect(){}
+    Rect(int a, int b);
+};
+
+class Triangle : public Rect
+{
+public:
+    Triangle(){}
+    Triangle(int a, int b, int c);
+    void S() override;
+    void P() override;
+private:
+    int c = 0;
+};
+
+class Circle : public Triangle
+{
+public:
+    Circle(){}
+    Circle(int a) :r(a){}
+    void S() override;
+    void P() override;
+private:
+    int r;
+};
+
+class Ellipse : public Circle
+{
+public:
+    Ellipse(){}
+    Ellipse(int a, int b);
+    void S() override;
+    void P() override;
+
+};
+
+
+Square::Square(int a)
+{
+    this->a = a;
+    this->b = a;
+    this->angles = 4;
+}
+
+void Square::S()
+{
+    this->s = a * b;
+}
+
+void Square::P()
+{
+    this->p = (a + b) * 2;
+}
+
+int Square::GetAngles()
+{
+    return this->angles;
+}
+
+int Square::GetPerimetr()
+{
+    return this->p;
+}
+
+int Square::GetSquare()
+{
+    return this->s;
+}
+
+Rect::Rect(int a, int b)
+{
+    this->a = a;
+    this->b = b;
+}
+
+Triangle::Triangle(int a, int b, int c)
+{
+    this->a = a;
+    this->b = b;
+    this->c = c;
+    this->angles = 3;
+}
+
+void Triangle::S()
+{
+    auto pp = (a + b + c) / 2;
+    this->s = std::sqrt(pp*(pp - a)*(pp - b)*(pp - c));
+}
+
+void Triangle::P()
+{
+    this->p = a + b + c;
+}
+
+void Circle::S()
+{
+    this->s = 3, 14 * r * r;
+}
+
+void Circle::P()
+{
+    this->p = 2 * 3, 14 * r;
+}
+
+Ellipse::Ellipse(int a, int b)
+{
+    this->a = a;
+    this->b = b;
+}
+
+void Ellipse::S()
+{
+    this->s = 3, 14 * a * b;
+}
+
+void Ellipse::P()
+{
+    this->p = std::sqrt((a * a + b * b) / 2) * 2 * 3, 14;
+}
+
+void angles_amount(Figure* p) 
+{
+    std::cout << p->GetAngles() << std::endl;
+}
+void perimetr(Figure* p)
+{
+    std::cout << p->GetPerimetr() << std::endl;
+}
+void square(Figure* p)
+{
+    std::cout << p->GetSquare() << std::endl;
+}
+int main()
+{
+
+    Rect a(5, 4);
+    a.S();
+
+
+}
---
+++ task_5/task_5/task_5.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+using namespace std;
+
+void register1(string& arr)
+{
+	for (auto &element : arr)
+	{
+		if (element > 'A' && element < 'Z') element += 'z' - 'Z';
+		if (element > 'А' && element < 'Я') element += 'я' - 'Я';
+	}
+	
+}
+void file_of(const string& arr, const string& c)
+{
+	ofstream fout;
+	fout.exceptions(ofstream::badbit | ofstream::failbit);
+	try
+	{
+		fout.open(c, ofstream::app);
+		fout << arr << endl;
+		fout.close();
+	}
+	catch (const ofstream::failure& ex)
+	{
+		cerr << ex.what() << endl;
+		cerr << ex.code() << endl;
+		cerr << "Error to write file"<< c << endl;
+		cout << "=========================" << endl;
+	}
+}
+
+int main()
+{
+	string MyFile = "MyFile.txt";
+    string c = "processed//c.txt";
+	string even = "processed//even.txt";
+	string odd = "processed//odd.txt";
+	string directory = "processed";
+	string str;
+	try
+	{
+		std::filesystem::create_directory(directory);
+	}
+	catch (const std::filesystem::filesystem_error& ex)
+	{
+		cerr << ex.what() << endl;
+		cerr << ex.code() << endl;
+		cerr << "Error to create directory" << endl;
+		cout << "=========================" << endl;
+	}
+	ofstream fout;
+	ifstream fin;
+	fout.exceptions(ofstream::badbit | ofstream::failbit);
+	fin.exceptions(ifstream::badbit | ifstream::failbit);
+	try
+	{
+		fout.open(MyFile);
+		cout << "Enter the text in MyFile for further sorting\nEnter 1 to stop writing:";
+		while (true)
+		{
+			str = "";
+			cin >> str;
+			if (str == "1")
+			{
+				break;
+			}
+			fout << str << endl;
+		}
+		fout.close();
+	}
+	catch (const ofstream::failure& ex)
+	{
+		cerr << ex.what() << endl;
+		cerr << ex.code() << endl;
+		cerr << "Error to write file MyFile" << endl;
+		cout << "=========================" << endl;
+	}
+	
+	try
+	{
+		
+		fin.open(MyFile);
+		while (!fin.eof())			//как я понял жалуется на eof, как исправить я не в курсе
+		{
+			str = "";
+			fin >> str;
+			register1(str);
+			int s = str.find("c", 0);
+			if (s >= 0)
+			{
+				str[s] = 'C';
+				file_of(str, c);
+			}
+			else
+			{
+				if (str.size() % 2 == 0)
+				{
+					str.push_back('_');
+					file_of(str, even);
+				}
+				else
+				{
+					str.push_back('_');
+					file_of(str, odd);
+				}
+			}
+		}
+		fin.close();
+	}
+	catch (const ifstream::failure& ex)
+	{
+		cerr << ex.what() << endl;
+		cerr << ex.code() << endl;
+		cerr << "Error to read file MyFile" << endl;
+		cout << "=========================" << endl;
+	}
+
+
+   
+
+	
+    
+}
